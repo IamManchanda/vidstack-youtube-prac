f4f0b06144363308325549a865e43a5a0ebbc65a
Harry Manchanda <harmanmanchanda182@gmail.com>
2024-04-24 01:22:15 +0530

    Youtube and vimeo

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,21 @@ import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/l
 export default function Home() {
   return (
     <div className='flex items-center justify-center w-full h-lvh bg-slate-500'>
-      <div className='w-9/12'>
-        <MediaPlayer src="vimeo/937742035">
-          <MediaProvider />
-          <DefaultVideoLayout icons={defaultLayoutIcons} />
-        </MediaPlayer>
+      <div className='flex w-10/12 gap-12'>
+        <div className="w-full h-full">
+          <p className='text-white'>YouTube</p>
+          <MediaPlayer src="youtube/_cMxraX_5RE">
+            <MediaProvider />
+            <DefaultVideoLayout icons={defaultLayoutIcons} />
+          </MediaPlayer>
+        </div>
+        <div className="w-full h-full">
+          <p className='text-white'>Vimeo</p>
+          <MediaPlayer src="vimeo/640499893">
+            <MediaProvider />
+            <DefaultVideoLayout icons={defaultLayoutIcons} />
+          </MediaPlayer>
+        </div>
       </div>
     </div>
   );
